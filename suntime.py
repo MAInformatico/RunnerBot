e5ec426c2ac318ed72c26e932dd63b287cebd91b
Mike <magutierrezsistemas@gmail.com>
2021-10-24 19:28:54 +0200

    updating timezone hour

--- suntime.py
+++ suntime.py
@@ -1,15 +1,23 @@
 import datetime
 import ephem #dependecy required. To install it, run 'pip install pyephem'
 
+
 def getSunrise():
     o = ephem.Observer()
     o.lat, o.long, o.date = '37:10', '-3:36', datetime.datetime.utcnow() #Set your own coordinates here
     Sun = ephem.Sun(o)
-    return str(o.next_rising(Sun))
+    return getTimezone(str(o.next_rising(Sun)))
 
 def getSunset():
     o = ephem.Observer()
     o.lat, o.long, o.date = '37:10', '-3:36', datetime.datetime.utcnow() #Same situation than previous method
     Sun = ephem.Sun(o)
-    return str(o.next_setting(Sun))
+    return getTimezone(str(o.next_setting(Sun)))
 
+def getTimezone(time):
+    splitTime = time.split(' ')
+    hour = datetime.datetime.strptime(splitTime[1],'%X')
+    hour += datetime.timedelta(hours=2)
+    splitHour = str(hour).split(' ')
+    timezone = splitTime[0] + " " + str(splitHour[1])
+    return str(timezone)
